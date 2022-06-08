a211a5312e9a5f11244deede2fe696e4f1c41db9
Heritier LIONGE <heritierlionge@yahoo.com>
2022-06-08 12:41:44 +0200

    création de la fonction

---
+++ ProducSign.js
@@ -0,0 +1,11 @@
+function productSign(n, m) {
+    let produit = n * m;
+    if (produit > 0) {
+        console.log(1);
+    } else if (produit < 0) {
+        console.log(-1)
+    } else {
+        console.log(0)
+    }
+
+}
